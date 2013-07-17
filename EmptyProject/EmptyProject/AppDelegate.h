3c8fb8c9d89141bd45ce63740fbb5ebd93572900
trubodetal <Maksimenko.M@trubodetal.ru>
2013-07-17 11:43:25 +0600

    Добавил файлы( это заголовочный текст)
    Добавил файлы (это детальный текст)

---
+++ EmptyProject/EmptyProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EmptyProject
+//
+//  Created by Admin on 7/17/13.
+//  Copyright (c) 2013 ru.trubodetal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EmptyProject/EmptyProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EmptyProject
+//
+//  Created by Admin on 7/17/13.
+//  Copyright (c) 2013 ru.trubodetal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
